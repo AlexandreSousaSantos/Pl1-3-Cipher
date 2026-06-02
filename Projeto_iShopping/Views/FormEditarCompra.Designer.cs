c06c85860a5a706560bc28dcb60a1ef3c6f83c60
catialeocadio <2025182962@my.ipleiria.pt>
2026-06-02 22:01:34 +0100

    Começei a fazer o FormEditarCompra hoje

--- Projeto_iShopping/Views/FormEditarCompra.Designer.cs
+++ Projeto_iShopping/Views/FormEditarCompra.Designer.cs
@@ -30,144 +30,160 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNomeCompra = new System.Windows.Forms.TextBox();
             this.DGV_Historico_de_Orcamentos = new System.Windows.Forms.DataGridView();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxArtigos = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.numQuantidade = new System.Windows.Forms.NumericUpDown();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnGuardarCompra = new System.Windows.Forms.Button();
+            this.btnRemoverItem = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Historico_de_Orcamentos)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantidade)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(206, 26);
+            this.label1.Size = new System.Drawing.Size(265, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Criar/Editar Compra";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 54);
+            this.label2.Location = new System.Drawing.Point(19, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.Size = new System.Drawing.Size(114, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nome da Compra";
             // 
-            // textBox1
+            // txtNomeCompra
             // 
-            this.textBox1.Location = new System.Drawing.Point(17, 70);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(344, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtNomeCompra.Location = new System.Drawing.Point(23, 86);
+            this.txtNomeCompra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNomeCompra.Name = "txtNomeCompra";
+            this.txtNomeCompra.Size = new System.Drawing.Size(457, 22);
+            this.txtNomeCompra.TabIndex = 2;
             // 
             // DGV_Historico_de_Orcamentos
             // 
             this.DGV_Historico_de_Orcamentos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_Historico_de_Orcamentos.Location = new System.Drawing.Point(17, 134);
+            this.DGV_Historico_de_Orcamentos.Location = new System.Drawing.Point(23, 165);
+            this.DGV_Historico_de_Orcamentos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DGV_Historico_de_Orcamentos.Name = "DGV_Historico_de_Orcamentos";
-            this.DGV_Historico_de_Orcamentos.Size = new System.Drawing.Size(748, 250);
+            this.DGV_Historico_de_Orcamentos.RowHeadersWidth = 51;
+            this.DGV_Historico_de_Orcamentos.Size = new System.Drawing.Size(997, 308);
             this.DGV_Historico_de_Orcamentos.TabIndex = 3;
             // 
-            // comboBox1
+            // comboBoxArtigos
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(97, 405);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(275, 21);
-            this.comboBox1.TabIndex = 5;
+            this.comboBoxArtigos.FormattingEnabled = true;
+            this.comboBoxArtigos.Location = new System.Drawing.Point(129, 498);
+            this.comboBoxArtigos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxArtigos.Name = "comboBoxArtigos";
+            this.comboBoxArtigos.Size = new System.Drawing.Size(365, 24);
+            this.comboBoxArtigos.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(387, 408);
+            this.label3.Location = new System.Drawing.Point(515, 504);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.Size = new System.Drawing.Size(30, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "Qty:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(39, 406);
+            this.label4.Location = new System.Drawing.Point(52, 500);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.Size = new System.Drawing.Size(45, 16);
             this.label4.TabIndex = 7;
             this.label4.Text = "Artigo:";
             // 
-            // numericUpDown1
+            // numQuantidade
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(420, 405);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 8;
+            this.numQuantidade.Location = new System.Drawing.Point(559, 500);
+            this.numQuantidade.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numQuantidade.Name = "numQuantidade";
+            this.numQuantidade.Size = new System.Drawing.Size(160, 22);
+            this.numQuantidade.TabIndex = 8;
             // 
-            // button1
+            // btnAdicionar
             // 
-            this.button1.Location = new System.Drawing.Point(553, 406);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(78, 19);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Adicionar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Location = new System.Drawing.Point(787, 498);
+            this.btnAdicionar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(104, 25);
+            this.btnAdicionar.TabIndex = 9;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnGuardarCompra
             // 
-            this.button2.Location = new System.Drawing.Point(635, 93);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(117, 35);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Guardar Compra";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnGuardarCompra.Location = new System.Drawing.Point(864, 86);
+            this.btnGuardarCompra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGuardarCompra.Name = "btnGuardarCompra";
+            this.btnGuardarCompra.Size = new System.Drawing.Size(156, 43);
+            this.btnGuardarCompra.TabIndex = 10;
+            this.btnGuardarCompra.Text = "Guardar Compra";
+            this.btnGuardarCompra.UseVisualStyleBackColor = true;
+            this.btnGuardarCompra.Click += new System.EventHandler(this.btnGuardarCompra_Click);
             // 
-            // button3
+            // btnRemoverItem
             // 
-            this.button3.Location = new System.Drawing.Point(674, 407);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(91, 19);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Remover item";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnRemoverItem.Location = new System.Drawing.Point(899, 498);
+            this.btnRemoverItem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRemoverItem.Name = "btnRemoverItem";
+            this.btnRemoverItem.Size = new System.Drawing.Size(121, 25);
+            this.btnRemoverItem.TabIndex = 11;
+            this.btnRemoverItem.Text = "Remover item";
+            this.btnRemoverItem.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 104);
+            this.label5.Location = new System.Drawing.Point(19, 128);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.Size = new System.Drawing.Size(94, 16);
             this.label5.TabIndex = 12;
             this.label5.Text = "Itens Previstos";
             // 
             // FormEditarCompra
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnRemoverItem);
+            this.Controls.Add(this.btnGuardarCompra);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.numQuantidade);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBoxArtigos);
             this.Controls.Add(this.DGV_Historico_de_Orcamentos);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtNomeCompra);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormEditarCompra";
             this.Text = "FormEditarCompra";
+            this.Load += new System.EventHandler(this.FormEditarCompra_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Historico_de_Orcamentos)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantidade)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,15 +193,15 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNomeCompra;
         private System.Windows.Forms.DataGridView DGV_Historico_de_Orcamentos;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxArtigos;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numQuantidade;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnGuardarCompra;
+        private System.Windows.Forms.Button btnRemoverItem;
         private System.Windows.Forms.Label label5;
     }
 }
--- Projeto_iShopping/Views/FormEditarCompra.cs
+++ Projeto_iShopping/Views/FormEditarCompra.cs
@@ -7,14 +7,195 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Projeto_iShopping.Models;
+using Projeto_iShopping.Controller;
 
 namespace Projeto_iShopping.Views
 {
     public partial class FormEditarCompra : Form
     {
-        public FormEditarCompra()
+        // Variável para armazenar o ID da compra a ser editada
+        // Usamos o _ para indicar que é uma variável privada da classe
+        private int _compraId;
+
+        // Variável para indicar se estamos em modo de edição ou criação
+        private bool _isEdicao;
+
+        // Construtor para criar uma nova lista de compras
+        public FormEditarCompra() 
         {
-            InitializeComponent();
+            InitializeComponent(); // Inicializa os componentes do formulário
+            _compraId = 0; // Inicializa o ID da compra como 0 (indica criação)
+            _isEdicao = false; // Inicializa o modo de edição como falso (indica criação)
+        }
+
+        // Construtor para editar uma lista existente
+        public FormEditarCompra(int compraId)
+        {
+            InitializeComponent(); // Inicializa os componentes do formulário
+            _compraId = compraId; // Define o ID da compra a ser editada
+            _isEdicao = true; // Define o modo de edição como verdadeiro (indica edição)
+        }
+
+        private void FormEditarCompra_Load(object sender, EventArgs e)
+        {
+            CarregarArtigosDisponiveis();
+
+            if (_isEdicao)
+            {
+                CarregaDadosCompra();
+                AtualizarGrelha();
+            }
+            else
+            {
+                ConfigurarModoNovaCompra();
+            }
+        }
+
+        // Método para carregar os dados da compra a ser editada
+        private void ConfigurarModoNovaCompra()
+        {
+            btnAdicionar.Enabled = false; // Desativa o botão de adicionar itens, pois ainda não há uma compra criada
+            btnRemoverItem.Enabled = false; // Desativa o botão de remover itens, pois não há itens para remover
+            comboBoxArtigos.Enabled = false; // Desativa o comboBox de artigos, pois ainda não há uma compra criada
+            numQuantidade.Enabled = false; // Desativa o controle de quantidade, pois ainda não há uma compra criada
+        }
+
+        // Método para carregar os dados da compra a ser editada
+        private void CarregarArtigosDisponiveis()
+        {
+            // Carrega os artigos disponíveis para seleção no comboBox
+            using (iShoppingContext db = new iShoppingContext())
+            {
+                // Consulta os artigos disponíveis, ordenados por nome, e seleciona apenas o ID e o nome para exibição
+                var artigos = db.Artigos
+                    .OrderBy(a => a.NomeArtigo) // Ordena os artigos por nome
+                    .Select(a => new { a.Id, Nome = a.NomeArtigo }) // Seleciona apenas o ID e o nome do artigo
+                    .ToList(); // Converte o resultado para uma lista
+                comboBoxArtigos.DataSource = artigos; // Define a fonte de dados do comboBox como a lista de artigos
+                comboBoxArtigos.DisplayMember = "Nome"; // Define o membro a ser exibido no comboBox como o nome do artigo
+                comboBoxArtigos.ValueMember = "Id"; // Define o membro a ser usado como valor no comboBox como o ID do artigo
+            }
+        }
+
+        private void CarregarDadosCompra()
+        {
+            // Carrega os dados da compra a ser editada
+            using (iShoppingContext db = new iShoppingContext())
+            {
+                // Consulta a compra pelo ID e preenche os campos do formulário com os dados da compra
+                Compra compra = db.Compras.Find(_compraId);
+
+                // Verifica se a compra foi encontrada no banco de dados
+                if (compra != null)
+                {
+                    // Preenche o campo de nome da compra com o nome da compra encontrada
+                    txtNomeCompra.Text = compra.NomeCompra;
+
+                    // Verifica se a compra está fechada, ou seja,
+                    // se o campo FechadaPorId tem um valor
+                    if (compra.FechadaPorId)
+                    {
+                        BloquearFormularioLeitura();
+                    }
+                    else
+                    {
+                        btnAdicionar.Enabled = true; // Ativa o botão de adicionar itens, pois a compra está aberta
+                        btnRemoverItem.Enabled = true; // Ativa o botão de remover itens, pois a compra está aberta
+                        comboBoxArtigos.Enabled = true; // Ativa o comboBox de artigos, pois a compra está aberta
+                        numQuantidade.Enabled = true; // Ativa o controle de quantidade, pois a compra está aberta
+                        btnGuardarCompra.Text = "Atualizar Nome"; // Altera o texto do botão de guardar compra para "Atualizar Nome",
+                                                                  // pois estamos a editar uma compra existente
+                    }
+                }
+            }
+        }
+
+        private void BloquearFormularioLeitura()
+        {
+            txtNomeCompra.Enabled = false; // Desativa o campo de nome da compra, pois a compra está fechada
+            btnGuardarCompra.Enabled = false; // Desativa o botão de guardar compra, pois a compra está fechada
+            comboBoxArtigos.Enabled = false; // Desativa o comboBox de artigos, pois a compra está fechada
+            numQuantidade.Enabled = false; // Desativa o controle de quantidade, pois a compra está fechada
+            btnAdicionar.Enabled = false; // Desativa o botão de adicionar itens, pois a compra está fechada
+            btnRemoverItem.Enabled = false; // Desativa o botão de remover itens, pois a compra está fechada
+        
+            // Exibe uma mensagem
+            MessageBox.Show("Esta compra está fechada e não pode ser editada só pode ser vista.", "Compra Fechada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Método para atualizar a grelha de itens de compra
+        private void AtualizarGrelha()
+        {
+            // Atualiza a grelha de itens de compra e mostra
+            // os itens relacionados à compra atual
+            using (iShoppingContext db = new iShoppingContext())
+            {
+                // Consulta os itens de compra relacionados à compra atual,
+                // filtrando pelo ID da compra
+                var itens = db.ItemCompra
+                    .Where(ic => ic.CompraId == _compraId) // Filtra os itens de compra pelo ID da compra
+                    .Select(ic => new // Seleciona os dados abaixo
+                    {
+                        Id = ic.Id, // Seleciona o ID do item de compra
+                        Artigo = ic.Artigo.NomeArtigo, // Seleciona o nome do artigo relacionado ao item de compra
+                        Quantidade = ic.QuantidadePrevista // Seleciona a quantidade prevista do item de compra
+                    })
+                    .ToList(); // Converte o resultado para uma lista
+                DGV_Historico_de_Orcamentos.DataSource = itens; // Define a fonte de dados do DataGridView
+                                                                // como a lista de itens de compra
+            }
+        }
+
+        private void btnGuardarCompra_Click(object sender, EventArgs e)
+        {
+            string nome = txtNomeCompra.Text.Trim(); // Obtém o nome da compra do campo de texto,
+                                                           // e retira espaços em branco
+
+            if (string.IsNullOrEmpty(nome)) // Verifica se o nome da compra está vazio
+            {
+                MessageBox.Show("O nome da compra não pode estar vazio.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error); // Exibe uma mensagem de erro
+                return; // O nome da compra é obrigatório, por isso sai do método
+            }
+
+            string mensagem; // recebe mensagem de retorno out da camada Controller
+
+            // Se o formulario estiver em modo criação de uma nova lista
+            if (_isEdicao)
+            {
+                int mesAtual = DateTime.Now.Month; // busca mês atual
+                int anoAtual = DateTime.Now.Year; // busca ano atual
+
+                // Envia os parâmetros para a camada lógica de negócio no controlador
+                bool sucesso = CompraController.CriarCompra(nome, mesAtual, anoAtual, out mensagem);
+
+                MessageBox.Show(mensagem, "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Se o controlador conseguir gravar o registo com sucesso
+                if (sucesso)
+                {
+                    using (iShoppingContext db = new iShoppingContext())
+                    {
+                        var novaCompra = db.Compras
+                            .OrderByDescending(c => c.Id) // Ordena pelo ID mais recente
+                            .FirstOrDefault(c => c.NomeCompra == nome); // Garante que o nome coincide
+                    
+                        if (novaCompra != null)
+                        {
+                            _compraId = novaCompra.Id; // Atualiza a variável da classe com o ID real da BD
+                            _isEdicao = true; // Muda o estado para modo de Edição
+
+                            btnAdicionar.Enabled = true;
+                            btnRemoverItem.Enabled = true;
+                            comboBoxArtigos.Enabled = true;
+                            numQuantidade.Enabled = true;
+                            btnGuardarCompra.Text = "Atualizar Nome"; // Modifica o texto do botão do topo
+                        }
+                    
+                    }
+                }
+            
+            }
         }
     }
 }
