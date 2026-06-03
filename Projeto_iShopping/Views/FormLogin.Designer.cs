364169048bb88ad587125f8251970caa985b9e84
catialeocadio <2025182962@my.ipleiria.pt>
2026-06-03 16:04:54 +0100

    Ajuste do FormLogin para ter mais segurança -> O campo de pasword agora oculta caracteres.

--- Projeto_iShopping/Views/FormLogin.Designer.cs
+++ Projeto_iShopping/Views/FormLogin.Designer.cs
@@ -40,9 +40,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
-            this.label1.Location = new System.Drawing.Point(303, 63);
+            this.label1.Location = new System.Drawing.Point(404, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 38);
+            this.label1.Size = new System.Drawing.Size(200, 47);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ishopping";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -51,42 +52,48 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label2.Location = new System.Drawing.Point(198, 168);
+            this.label2.Location = new System.Drawing.Point(264, 207);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.Size = new System.Drawing.Size(84, 22);
             this.label2.TabIndex = 1;
             this.label2.Text = "Utilizador";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // TButilizador
             // 
-            this.TButilizador.Location = new System.Drawing.Point(202, 196);
+            this.TButilizador.Location = new System.Drawing.Point(269, 241);
+            this.TButilizador.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TButilizador.Name = "TButilizador";
-            this.TButilizador.Size = new System.Drawing.Size(371, 20);
+            this.TButilizador.Size = new System.Drawing.Size(493, 22);
             this.TButilizador.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label3.Location = new System.Drawing.Point(199, 231);
+            this.label3.Location = new System.Drawing.Point(265, 284);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.Size = new System.Drawing.Size(89, 22);
             this.label3.TabIndex = 3;
             this.label3.Text = "Password";
             // 
             // TBpassword
             // 
-            this.TBpassword.Location = new System.Drawing.Point(202, 260);
+            this.TBpassword.Location = new System.Drawing.Point(269, 320);
+            this.TBpassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TBpassword.Name = "TBpassword";
-            this.TBpassword.Size = new System.Drawing.Size(371, 20);
+            this.TBpassword.PasswordChar = '*';
+            this.TBpassword.Size = new System.Drawing.Size(493, 22);
             this.TBpassword.TabIndex = 4;
             // 
             // btentrar
             // 
-            this.btentrar.Location = new System.Drawing.Point(348, 344);
+            this.btentrar.Location = new System.Drawing.Point(464, 423);
+            this.btentrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btentrar.Name = "btentrar";
-            this.btentrar.Size = new System.Drawing.Size(82, 32);
+            this.btentrar.Size = new System.Drawing.Size(109, 39);
             this.btentrar.TabIndex = 5;
             this.btentrar.Text = "Entrar";
             this.btentrar.UseVisualStyleBackColor = true;
@@ -94,15 +101,16 @@
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.btentrar);
             this.Controls.Add(this.TBpassword);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.TButilizador);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormLogin";
             this.Text = "FormLogin";
             this.Load += new System.EventHandler(this.FormLogin_Load);
