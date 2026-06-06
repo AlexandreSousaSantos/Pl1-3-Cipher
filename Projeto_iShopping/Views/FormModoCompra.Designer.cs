1a1c77a64d50c35ad8bb82fdfe94b6c633aa1e17
AlexandreSousaSantos <2025191285@my.ipleiria.pt>
2026-06-06 14:51:13 +0100

    ModoCompra

--- Projeto_iShopping/Views/FormModoCompra.Designer.cs
+++ Projeto_iShopping/Views/FormModoCompra.Designer.cs
@@ -28,12 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DgvModocompra = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NudQtAdquirida = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnMarcarAquirido = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbArtigo = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.NudQt = new System.Windows.Forms.NumericUpDown();
+            this.NudPreço = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnFecharCompra = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvModocompra)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudQtAdquirida)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudQt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudPreço)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvModocompra
+            // 
+            this.DgvModocompra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvModocompra.Location = new System.Drawing.Point(18, 56);
+            this.DgvModocompra.Name = "DgvModocompra";
+            this.DgvModocompra.Size = new System.Drawing.Size(829, 241);
+            this.DgvModocompra.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Marcar como adquirido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 357);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Qt Adquirida:";
+            // 
+            // NudQtAdquirida
+            // 
+            this.NudQtAdquirida.Location = new System.Drawing.Point(98, 355);
+            this.NudQtAdquirida.Name = "NudQtAdquirida";
+            this.NudQtAdquirida.Size = new System.Drawing.Size(76, 20);
+            this.NudQtAdquirida.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Preço Unit:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(246, 357);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(76, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // btnMarcarAquirido
+            // 
+            this.btnMarcarAquirido.Location = new System.Drawing.Point(363, 350);
+            this.btnMarcarAquirido.Name = "btnMarcarAquirido";
+            this.btnMarcarAquirido.Size = new System.Drawing.Size(114, 27);
+            this.btnMarcarAquirido.TabIndex = 6;
+            this.btnMarcarAquirido.Text = "Marcar Adquirido";
+            this.btnMarcarAquirido.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Adicionar Item Não Previsto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 426);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Artigo";
+            // 
+            // cmbArtigo
+            // 
+            this.cmbArtigo.FormattingEnabled = true;
+            this.cmbArtigo.Location = new System.Drawing.Point(72, 421);
+            this.cmbArtigo.Name = "cmbArtigo";
+            this.cmbArtigo.Size = new System.Drawing.Size(152, 21);
+            this.cmbArtigo.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(255, 424);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Qt:";
+            // 
+            // NudQt
+            // 
+            this.NudQt.Location = new System.Drawing.Point(292, 421);
+            this.NudQt.Name = "NudQt";
+            this.NudQt.Size = new System.Drawing.Size(76, 20);
+            this.NudQt.TabIndex = 11;
+            // 
+            // NudPreço
+            // 
+            this.NudPreço.Location = new System.Drawing.Point(449, 424);
+            this.NudPreço.Name = "NudPreço";
+            this.NudPreço.Size = new System.Drawing.Size(76, 20);
+            this.NudPreço.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(392, 423);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Preço:";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(550, 417);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(114, 27);
+            this.btnAdicionar.TabIndex = 14;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            // 
+            // btnFecharCompra
+            // 
+            this.btnFecharCompra.Location = new System.Drawing.Point(711, 460);
+            this.btnFecharCompra.Name = "btnFecharCompra";
+            this.btnFecharCompra.Size = new System.Drawing.Size(114, 27);
+            this.btnFecharCompra.TabIndex = 15;
+            this.btnFecharCompra.Text = "Fechar Compra";
+            this.btnFecharCompra.UseVisualStyleBackColor = true;
+            // 
+            // FormModoCompra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(859, 499);
+            this.Controls.Add(this.btnFecharCompra);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.NudPreço);
+            this.Controls.Add(this.NudQt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbArtigo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnMarcarAquirido);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NudQtAdquirida);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DgvModocompra);
+            this.Name = "FormModoCompra";
             this.Text = "FormsModoCompra";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvModocompra)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudQtAdquirida)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudQt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudPreço)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView DgvModocompra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown NudQtAdquirida;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnMarcarAquirido;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbArtigo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown NudQt;
+        private System.Windows.Forms.NumericUpDown NudPreço;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnFecharCompra;
     }
 }
